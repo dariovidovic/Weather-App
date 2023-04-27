23862fbec8360a379ff7a5cdcd0680adfc3a6f6b
dariovidovic <malivida00@gmail.com>
2023-04-27 19:39:17 +0200

    Finish fragment_my_cities.xml

--- app/src/main/java/com/example/weatherapp/ui/MyCitiesFragment.kt
+++ app/src/main/java/com/example/weatherapp/ui/MyCitiesFragment.kt
@@ -8,33 +8,17 @@ import android.view.ViewGroup
 import com.example.weatherapp.R
 
 class MyCitiesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_my_cities, container, false)
     }
 
     companion object {
 
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MyCitiesFragment().apply {
-                arguments = Bundle().apply {
-
-                }
-            }
+        fun newInstance() = MyCitiesFragment()
     }
+
 }
